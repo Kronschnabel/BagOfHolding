3f8e7c850988f6372eb6e08a9d51c365516ec1fd
Matt Kronschnabel <kronsch@okstate.edu>
2018-08-23 10:19:13 -0500

    Color changes now save themselves.

--- BagOfHolding/Settings.cs
+++ BagOfHolding/Settings.cs
@@ -11,14 +11,20 @@
         public Settings() {
             // // To add event handlers for saving and changing settings, uncomment the lines below:
             //
-            // this.SettingChanging += this.SettingChangingEventHandler;
+            //this.SettingChanging += this.SettingChangingEventHandler;
+            //
+            PropertyChanged += PropertyChangedEventHandler;
             //
             // this.SettingsSaving += this.SettingsSavingEventHandler;
             //
         }
         
         private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e) {
-            // Add code to handle the SettingChangingEvent event here.
+            
+        }
+
+        private void PropertyChangedEventHandler(object sender, System.ComponentModel.PropertyChangedEventArgs e) {
+            Save();
         }
         
         private void SettingsSavingEventHandler(object sender, System.ComponentModel.CancelEventArgs e) {
